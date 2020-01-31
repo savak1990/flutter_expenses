a3344d8166bc9f0807669002f9ea1a120bcf5a02
savak1990 <slava.klovan@gmai.com>
2020-01-31 11:38:20 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/new_transaction.dart';
+import 'widgets/transaction_list.dart';
+import 'widgets/user_transactions.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
+      ),
+      body: UserTransactions(),
+    );
+  }
+}
